8b9d5365481aadf4d1bfd889e57dea0c645c81d4
Or mizrahi <108184274+OrMizrahi12@users.noreply.github.com>
2022-10-27 09:29:16 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,16 +4,8 @@ const BASE_URL = 'http://localhost:3500';
 export default axios.create({
     baseURL: BASE_URL
 });
-
-// #14 we create new axios 
-// we going use with intersoptor,
-//  that will use for refresh the token for geting new expire 
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
-    // this code will be attach in all request, 
-    // for success the request 
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
-
-//  for #15 go to useAxiosPrivet.js
